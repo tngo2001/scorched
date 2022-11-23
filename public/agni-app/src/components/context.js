1a0ae2258d60ef127ac5ec9b545e2fc6688e2dec
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-11-23 14:47:20 -0800

    add layout to context page

--- public/agni-app/src/components/context.js
+++ public/agni-app/src/components/context.js
@@ -3,6 +3,16 @@ export const Context = () => {
     return (
       <div className="section">
         <h3>Context page</h3>
+
+        <p>The number of wildfires that have occurred have been relatively constant</p>
+
+        <p> put graph here</p>
+
+
+
+        <p> Yet, why does it feel like you've seen wildfires on the news more and more in the past few years?</p>
+
+        <p>Let's smoke out the data</p>
        
       </div>);
   };
