90ba834f7de256f806cbe82c454d24ba7d75b85d
tngo2001 <tngo2001@uw.edu>
2022-11-27 23:20:01 -0800

    Added second map and buttons

--- public/agni-app/src/components/location.js
+++ public/agni-app/src/components/location.js
@@ -1,42 +1,92 @@
 import { Helmet } from "react-helmet"
+import { useRef } from "react";
 
 export const Location = () => {
+    const acresRef = useRef(null);
+    const firesRef = useRef(null);
+
+    const showAcres = () => {
+      acresRef.current.className = "";
+      firesRef.current.className = "hidden";
+    }
+
+    const showFires = () => {
+      acresRef.current.className = "hidden";
+      firesRef.current.className = "";
+    }
+
     return (
       <div className="section dark-bg">
         <h3>Fires, fires everywhere</h3>
 
-        <div className='tableauPlaceholder' id='viz1669606140332' style={{margin: 'auto'}}>
-          <noscript>
-            <a href='/#'>
-              <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;7C&#47;7CNN6FNKB&#47;1_rss.png' style={{border: 'none'}} />
-            </a>
-          </noscript>
-          <object className='tableauViz' style={{display: 'none'}}>
-              <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-              <param name='embed_code_version' value='3' />
-              <param name='path' value='shared&#47;7CNN6FNKB' />
-              <param name='toolbar' value='yes' />
-              <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;7C&#47;7CNN6FNKB&#47;1.png' />
-              <param name='animate_transition' value='yes' />
-              <param name='display_static_image' value='yes' />
-              <param name='display_spinner' value='yes' />
-              <param name='display_overlay' value='yes' />
-              <param name='display_count' value='yes' />
-              <param name='language' value='en-US' />
-          </object>
+        <input type="radio" value="Acres" name="chart" defaultChecked onClick={showAcres}/> Total Acres Burned
+        <input type="radio" value="Fires" name="chart" onClick={showFires} /> Total Fires
+
+        <div ref={acresRef}>
+          <div className='tableauPlaceholder' id='viz1669606140332' style={{margin: 'auto'}}>
+            <noscript>
+              <a href='/#'>
+                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;7C&#47;7CNN6FNKB&#47;1_rss.png' style={{border: 'none'}} />
+              </a>
+            </noscript>
+            <object className='tableauViz' style={{display: 'none'}}>
+                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                <param name='embed_code_version' value='3' />
+                <param name='path' value='shared&#47;7CNN6FNKB' />
+                <param name='toolbar' value='yes' />
+                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;7C&#47;7CNN6FNKB&#47;1.png' />
+                <param name='animate_transition' value='yes' />
+                <param name='display_static_image' value='yes' />
+                <param name='display_spinner' value='yes' />
+                <param name='display_overlay' value='yes' />
+                <param name='display_count' value='yes' />
+                <param name='language' value='en-US' />
+            </object>
+          </div>
+          <Helmet>
+            <script type='text/javascript'>
+              var divElement = document.getElementById('viz1669606140332');
+              var vizElement = divElement.getElementsByTagName('object')[0];
+              vizElement.style.width = '100vh';
+              vizElement.style.height = '75vh';
+              var scriptElement = document.createElement('script');
+              scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+              vizElement.parentNode.insertBefore(scriptElement, vizElement);
+            </script>
+          </Helmet>
         </div>
 
-        <Helmet>
-          <script type='text/javascript'>
-            var divElement = document.getElementById('viz1669606140332');
-            var vizElement = divElement.getElementsByTagName('object')[0];
-            vizElement.style.width = '100vh';
-            vizElement.style.height = '75vh';
-            var scriptElement = document.createElement('script');
-            scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-            vizElement.parentNode.insertBefore(scriptElement, vizElement);
-          </script>
-        </Helmet>
+        <div ref={firesRef} className='hidden'>
+          <div className='tableauPlaceholder' id='viz1669614729428' style={{margin: 'auto'}}>
+            <noscript><a href='/#'><img alt='Which States Are Most Affected by Wildfires? ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Fires&#47;1_rss.png' style={{border: 'none'}} /></a></noscript>
+            <object className='tableauViz'  style={{display: 'none'}}>
+                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                <param name='embed_code_version' value='3' />
+                <param name='site_root' value='' />
+                <param name='name' value='CSE442FP&#47;Fires' />
+                <param name='tabs' value='no' />
+                <param name='toolbar' value='yes' />
+                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Fires&#47;1.png' />
+                <param name='animate_transition' value='yes' />
+                <param name='display_static_image' value='yes' />
+                <param name='display_spinner' value='yes' />
+                <param name='display_overlay' value='yes' />
+                <param name='display_count' value='yes' />
+                <param name='language' value='en-US' />
+            </object>
+          </div>
+          <Helmet>
+            <script type='text/javascript'>
+              var divElement = document.getElementById('viz1669614729428');
+              var vizElement = divElement.getElementsByTagName('object')[0];
+              vizElement.style.width = '100vh';
+              vizElement.style.height = '75vh';
+              var scriptElement = document.createElement('script');
+              scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+              vizElement.parentNode.insertBefore(scriptElement, vizElement);
+            </script>
+          </Helmet>
+        </div>
       </div>);
   };
 
