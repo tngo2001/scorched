80de6e07ae286b1ca008c0028993af8b39b6f77a
tngo2001 <tngo2001@uw.edu>
2022-11-28 13:30:06 -0800

    Modified map tooltips

--- public/agni-app/src/components/location.js
+++ public/agni-app/src/components/location.js
@@ -23,29 +23,32 @@ export const Location = () => {
         <input type="radio" value="Fires" name="chart" onClick={showFires} /> Total Fires
 
         <div ref={acresRef}>
-          <div className='tableauPlaceholder' id='viz1669606140332' style={{margin: 'auto'}}>
+          <div className='tableauPlaceholder' id='viz1669670537910' style={{margin: 'auto'}}>
             <noscript>
               <a href='/#'>
-                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;7C&#47;7CNN6FNKB&#47;1_rss.png' style={{border: 'none'}} />
+                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Acres&#47;1_rss.png' style={{border: 'none'}} />
               </a>
             </noscript>
             <object className='tableauViz' style={{display: 'none'}}>
                 <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                 <param name='embed_code_version' value='3' />
-                <param name='path' value='shared&#47;7CNN6FNKB' />
+                <param name='site_root' value='' />
+                <param name='name' value='CSE442FP&#47;Acres' />
+                <param name='tabs' value='no' />
                 <param name='toolbar' value='yes' />
-                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;7C&#47;7CNN6FNKB&#47;1.png' />
+                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Acres&#47;1.png' />
                 <param name='animate_transition' value='yes' />
                 <param name='display_static_image' value='yes' />
                 <param name='display_spinner' value='yes' />
                 <param name='display_overlay' value='yes' />
                 <param name='display_count' value='yes' />
                 <param name='language' value='en-US' />
+                <param name='filter' value='publish=yes' />
             </object>
           </div>
           <Helmet>
             <script type='text/javascript'>
-              var divElement = document.getElementById('viz1669606140332');
+              var divElement = document.getElementById('viz1669670537910');
               var vizElement = divElement.getElementsByTagName('object')[0];
               vizElement.style.width = '100vh';
               vizElement.style.height = '75vh';
@@ -57,9 +60,13 @@ export const Location = () => {
         </div>
 
         <div ref={firesRef} className='hidden'>
-          <div className='tableauPlaceholder' id='viz1669614729428' style={{margin: 'auto'}}>
-            <noscript><a href='/#'><img alt='Which States Are Most Affected by Wildfires? ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Fires&#47;1_rss.png' style={{border: 'none'}} /></a></noscript>
-            <object className='tableauViz'  style={{display: 'none'}}>
+          <div class='tableauPlaceholder' id='viz1669670721822' style={{margin: 'auto'}}>
+            <noscript>
+              <a href='/#'>
+                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Fires&#47;1_rss.png' style={{border: 'none'}} />
+              </a>
+            </noscript>
+            <object class='tableauViz' style={{display: 'none'}}>
                 <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                 <param name='embed_code_version' value='3' />
                 <param name='site_root' value='' />
@@ -73,11 +80,12 @@ export const Location = () => {
                 <param name='display_overlay' value='yes' />
                 <param name='display_count' value='yes' />
                 <param name='language' value='en-US' />
+                <param name='filter' value='publish=yes' />
             </object>
           </div>
           <Helmet>
             <script type='text/javascript'>
-              var divElement = document.getElementById('viz1669614729428');
+              var divElement = document.getElementById('viz1669670721822');
               var vizElement = divElement.getElementsByTagName('object')[0];
               vizElement.style.width = '100vh';
               vizElement.style.height = '75vh';
