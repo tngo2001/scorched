8794c0ea24577e4d4c142430757318855309f380
tngo2001 <tngo2001@uw.edu>
2022-11-27 20:46:00 -0800

    Removed comments and unnecessary files

--- public/agni-app/src/components/location.js
+++ public/agni-app/src/components/location.js
@@ -1,13 +1,9 @@
-// import firesMap from '../graph-placeholders/firesMap.png';
 import { Helmet } from "react-helmet"
 
 export const Location = () => {
     return (
       <div className="section dark-bg">
         <h3>Fires, fires everywhere</h3>
-
-        {/*<p>Which areas are most affected by wildfires?</p>
-        <img src={firesMap} alt="Contiguous US Map colored by severity of fires" width="70%" height="70%"/>*/}
 
         <div className='tableauPlaceholder' id='viz1669606140332' style={{margin: 'auto'}}>
           <noscript>
