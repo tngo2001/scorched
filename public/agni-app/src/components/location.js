a02a6ba53d744735b07fef919d83f24c972306d7
tngo2001 <tngo2001@uw.edu>
2022-11-28 21:40:37 -0800

    Changed color scale and made charts dark

--- public/agni-app/src/components/location.js
+++ public/agni-app/src/components/location.js
@@ -23,20 +23,20 @@ export const Location = () => {
         <input type="radio" value="Fires" name="chart" onClick={showFires} /> Total Fires
 
         <div ref={acresRef}>
-          <div className='tableauPlaceholder' id='viz1669670537910' style={{margin: 'auto'}}>
+          <div className='tableauPlaceholder' id='viz1669699694761' style={{margin: 'auto'}}>
             <noscript>
               <a href='/#'>
-                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Acres&#47;1_rss.png' style={{border: 'none'}} />
+                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sc&#47;ScorchedLocation&#47;Acres&#47;1_rss.png' style={{border: 'none'}} />
               </a>
             </noscript>
             <object className='tableauViz' style={{display: 'none'}}>
                 <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                 <param name='embed_code_version' value='3' />
                 <param name='site_root' value='' />
-                <param name='name' value='CSE442FP&#47;Acres' />
+                <param name='name' value='ScorchedLocation&#47;Acres' />
                 <param name='tabs' value='no' />
                 <param name='toolbar' value='yes' />
-                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Acres&#47;1.png' />
+                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sc&#47;ScorchedLocation&#47;Acres&#47;1.png' />
                 <param name='animate_transition' value='yes' />
                 <param name='display_static_image' value='yes' />
                 <param name='display_spinner' value='yes' />
@@ -48,10 +48,10 @@ export const Location = () => {
           </div>
           <Helmet>
             <script type='text/javascript'>
-              var divElement = document.getElementById('viz1669670537910');
+              var divElement = document.getElementById('viz1669699694761');
               var vizElement = divElement.getElementsByTagName('object')[0];
               vizElement.style.width = '100vh';
-              vizElement.style.height = '75vh';
+              vizElement.style.height = '500px';
               var scriptElement = document.createElement('script');
               scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
               vizElement.parentNode.insertBefore(scriptElement, vizElement);
@@ -60,20 +60,20 @@ export const Location = () => {
         </div>
 
         <div ref={firesRef} className='hidden'>
-          <div class='tableauPlaceholder' id='viz1669670721822' style={{margin: 'auto'}}>
+          <div className='tableauPlaceholder' id='viz1669699893163' style={{margin: 'auto'}}>
             <noscript>
               <a href='/#'>
-                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Fires&#47;1_rss.png' style={{border: 'none'}} />
+                <img alt='Which States Are Most Affected by Wildfires?' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sc&#47;ScorchedLocation&#47;Fires&#47;1_rss.png' style={{border: 'none'}} />
               </a>
             </noscript>
-            <object class='tableauViz' style={{display: 'none'}}>
+            <object className='tableauViz' style={{display: 'none'}}>
                 <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
                 <param name='embed_code_version' value='3' />
                 <param name='site_root' value='' />
-                <param name='name' value='CSE442FP&#47;Fires' />
+                <param name='name' value='ScorchedLocation&#47;Fires' />
                 <param name='tabs' value='no' />
                 <param name='toolbar' value='yes' />
-                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CS&#47;CSE442FP&#47;Fires&#47;1.png' />
+                <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sc&#47;ScorchedLocation&#47;Fires&#47;1.png' />
                 <param name='animate_transition' value='yes' />
                 <param name='display_static_image' value='yes' />
                 <param name='display_spinner' value='yes' />
@@ -85,10 +85,10 @@ export const Location = () => {
           </div>
           <Helmet>
             <script type='text/javascript'>
-              var divElement = document.getElementById('viz1669670721822');
+              var divElement = document.getElementById('viz1669699893163');
               var vizElement = divElement.getElementsByTagName('object')[0];
               vizElement.style.width = '100vh';
-              vizElement.style.height = '75vh';
+              vizElement.style.height = '500px';
               var scriptElement = document.createElement('script');
               scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
               vizElement.parentNode.insertBefore(scriptElement, vizElement);
